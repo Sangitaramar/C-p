30d62040a1efd96413151f61553f3d1ab6f081db
Sangitaramar <95616189+Sangitaramar@users.noreply.github.com>
2021-12-06 15:03:28 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,16 @@
 #include<stdio.h>
+#include<conio.h>
 void main(){
-printf("hello world");
+int a,b,c;
+clrscr();
+printf("enter the value of a and b");
+scanf("%d",&a);
+scanf("%d",&b);
+c=a+b;
+printf("\nthe sum of %d and %d = %d ",a,b,c);
+
+printf("\nthe sum of %d and %d = %d ",a,b,a+b);
+
+
+getch();
 }
